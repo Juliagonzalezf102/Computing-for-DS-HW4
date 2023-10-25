5ee6297fc37c743e7bff9f5b31974acf89364598
Guillem Mirabent Rubinat <guillem.mirabent@bse.eu>
2023-10-25 10:18:27 +0200

    update on GMR_Branch's exercises

--- hw4.py
+++ hw4.py
@@ -1,5 +1,5 @@
 ###########################################
-
+#%%
 #
 # 1. In this exercise we will make a "Patient" class
 #
@@ -18,14 +18,9 @@
 # the parameters should be stored as attributes
 # called "name" and "symptoms" respectively
 
-class Patient:
-    
-    def __init__(self, name, symptoms):
-        self.name = name
-        self.symptoms = symptoms
-    
-    
-
+'''
+solved below as a whole
+'''
 
 #
 # 1.2)
@@ -36,6 +31,9 @@ class Patient:
 #
 # This information should be stored somehow.
 
+'''
+solved below as a whole
+'''
 
 #
 # 1.3)
@@ -56,7 +54,36 @@ class Patient:
 #    following symptoms:
 #    ['fever', 'cough', 'anosmia']
 
+class Patient:
+    
+    def __init__(self, name: str, symptoms: list):
+        self.name = name
+        self.symptoms = symptoms
+        self.test = {}
+    
+    def add_test(self, test_name: str, test_results: bool):
+        self.test[test_name] = test_results
+    
+    def has_covid(self):
+        if 'covid' in self.test and self.test['covid']:
+            return 0.99
+        elif 'covid' in self.test and not self.test['covid']:
+            return 0.01
+        else:
+            prob = 0.05
+            for symptom in self.symptoms:
+                if symptom in ['fever', 'cough', 'anosmia']:
+                    prob += 0.1
+            return prob
 
+William = Patient('William', [#'fever',
+                            'cough',
+                            'anosmia'
+                            ])
+William.add_test('covid', False)
+print(William.has_covid())
+
+#%%
 ######################
 
 # 2. In this exercise you will make an English Deck class made of Card classes
@@ -75,8 +102,45 @@ class Patient:
 # Create a method called "shuffle" that shuffles the cards randomly. 
 # Create a method called "draw" that will draw a single card and print the suit and value. When a card is drawn, the card should be removed from the deck.
 
+import numpy as np
 
+class Card:
+    
+    def __init__(self, suit: str, value: str):
+        self.suit = suit
+        self.value = value
 
+class Deck:
+    
+    def __init__(self):
+        self.cards = []
+        for suit in ['Hearts', 'Diamonds', 'Clubs', 'Spades']:
+            for value in ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+                        'J', 'Q', 'K']:
+                self.cards.append(Card(suit, value))
+    
+    def shuffle(self):
+        np.random.shuffle(self.cards)
+    
+    def draw(self):
+        self.shuffle()
+        card = self.cards.pop()
+        #print(f'{card.value} of {card.suit}')
+        return f"{card.value} of {card.suit}"
+
+np.random.seed(168)
+english_deck = Deck()
+
+result = []
+while True:
+    result.append(english_deck.draw())
+    if len(english_deck.cards) == 0:
+        break
+
+print(len(result))
+display(result)
+
+#%%
 ###################
 
 # 3. In this exercise you will create an interface that will serve as template 
@@ -92,3 +156,4 @@ class Patient:
 
 # 3.3 Create a child class called "Circle" that inherits from "PlaneFigure" and has as parameters in the constructor "radius" (radius of the circle). Implement the abstract methods with the formula of the circle.
 
+@PlaneFigure.register
